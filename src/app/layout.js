4199862ee0b1c55a2cf1f07b045310b20a6899c6
Jamelle Messinger <jamswoof914@gmail.com>
2026-01-17 14:36:29 -0700

    add NavBar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+//Custom Components
+import NavBar from './components/NavBar';
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -20,6 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <NavBar/>
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-      <h1>Dashboard</h1>
+      <h1></h1>
     </>
   );
 }
