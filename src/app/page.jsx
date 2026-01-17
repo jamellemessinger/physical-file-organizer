d31978bd314c53a1f41341a48c962a3990abc016
Jamelle Messinger <jamswoof914@gmail.com>
2026-01-17 15:59:51 -0700

    remove jsx wireframing from page.jsx home component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <div id="file-navigation-menu">
-        <div id="collapse-navigation"></div>
-        <div id="all-files"></div>
-        <div id="filter">
-          <div id="tags"></div>
-          <div id="locations"></div>
-        </div>
-      </div>
-      <div id="container">
-        <div id="search-bar">search bar</div>
-        <div id="search-icon">Q</div>
-      </div>
-    </>
-  );
+  return <></>;
 }
