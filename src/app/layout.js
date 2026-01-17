caa197b9aab58e88b7cbbc3fe6e72b9b3909c0ce
Jamelle Messinger <jamswoof914@gmail.com>
2026-01-17 15:57:48 -0700

    remove NavBar component and import from layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
 //Custom Components
-import NavBar from '../components/NavBar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,7 +22,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <NavBar/>
         {children}
       </body>
     </html>
