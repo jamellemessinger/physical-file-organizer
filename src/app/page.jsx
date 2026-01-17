07060b68bec8bb4d6faf5707a4cb2ade85bc213f
Jamelle Messinger <jamswoof914@gmail.com>
2026-01-17 15:21:06 -0700

    JSX wireframe for the homepage/initial dashboard

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <>
+      <div id="file-navigation-menu">
+        <div id="collapse-navigation"></div>
+        <div id="all-files"></div>
+        <div id="filter">
+          <div id="tags"></div>
+          <div id="locations"></div>
+        </div>
+      </div>
+      <div id="container">
+        <div id="search-bar">search bar</div>
+        <div id="search-icon">Q</div>
+      </div>
+    </>
+  );
+}
