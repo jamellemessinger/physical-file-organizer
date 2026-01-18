d4b73216ebab398cc0e62ad6b896941c5db509ac
Jamelle Messinger <jamswoof914@gmail.com>
2026-01-17 20:40:35 -0700

    add CollapseSideBarButton component

--- src/components/file-navgation/FileSideBar.jsx
+++ src/components/file-navgation/FileSideBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AllFilesButton from './AllFilesButton';
 import FilterContainer from './FilterContainer';
+import CollapseSideBarButton from './CollapseSideBarButton';
 
 export default function SideBar() {
   return (
     <>
+      <CollapseSideBarButton />
       <AllFilesButton />
       <FilterContainer />
     </>
