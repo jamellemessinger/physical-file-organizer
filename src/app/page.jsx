95d834070820e7f78edf53f1e791cce0a92f3808
Jamelle Messinger <jamswoof914@gmail.com>
2026-01-18 09:58:03 -0700

    add SeachContainer.jsx to page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import FileSideBar from '@/components/file-navgation/FileSideBar';
+import SearchContainer from '@/components/search/SearchContainer';
 
 export default function Home() {
   return (
     <>
       <FileSideBar />
+      <SearchContainer />
     </>
   );
 }
