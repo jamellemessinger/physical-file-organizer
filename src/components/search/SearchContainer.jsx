7aac884f2320ca2c6acd4087ece84736c2db572e
Jamelle Messinger <jamswoof914@gmail.com>
2026-01-18 09:55:44 -0700

    add ProfileIcon.jsx to SearchContainer.jsx

--- src/components/search/SearchContainer.jsx
+++ src/components/search/SearchContainer.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import SearchBar from './SearchBar'
-import SearchBarButton from './SearchBarButton'
+import React from 'react';
+import SearchBar from './SearchBar';
+import SearchBarButton from './SearchBarButton';
+import ProfileIcon from '../user-navigation/ProfileIcon';
 
 export default function SearchContainer() {
   return (
     <>
-      <SearchBar/>
-      <SearchBarButton/>
+      <SearchBar />
+      <SearchBarButton />
+      <ProfileIcon />
     </>
-  )
+  );
 }
