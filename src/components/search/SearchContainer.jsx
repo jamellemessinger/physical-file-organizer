1aa4089eda6f9f3ce6655d9580a031adc217501f
Jamelle Messinger <jamswoof914@gmail.com>
2026-01-18 09:53:18 -0700

    add search components boilerplate

---
+++ src/components/search/SearchContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+import SearchBarButton from './SearchBarButton'
+
+export default function SearchContainer() {
+  return (
+    <>
+      <SearchBar/>
+      <SearchBarButton/>
+    </>
+  )
+}
