91baebc3a25c58f32915acc791a667141370f7e8
Jamelle Messinger <jamswoof914@gmail.com>
2026-01-20 22:45:41 -0700

    setup filterComponent for additional features

--- src/components/file-navgation/FilterComponent.jsx
+++ src/components/file-navgation/FilterComponent.jsx
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 
 export default function FilterComponent({ onClick, title, isExpanded, name }) {
   return (
-    <div className='flex justify-between py-2'>
-      <div>{title}</div>
-      <button onClick={() => onClick(name)}>{isExpanded ? '-' : '+'}</button>
+    <div>
+      <div className="flex justify-between py-2">
+        <div>{title}</div>
+        <button onClick={() => onClick(name)}>{isExpanded ? '-' : '+'}</button>
+      </div>
+      <div>
+        
+      </div>
     </div>
   );
 }
